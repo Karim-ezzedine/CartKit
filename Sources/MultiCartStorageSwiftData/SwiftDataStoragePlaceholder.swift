e019073b034a5e61452810036606c0ef6b7185eb
Karim Ezzedinne <karim.ezz70@gmail.com>
2025-11-27 23:55:50 +0200

    M1.3 – Configure availability & build settings

--- Sources/MultiCartStorageSwiftData/SwiftDataStoragePlaceholder.swift
+++ Sources/MultiCartStorageSwiftData/SwiftDataStoragePlaceholder.swift
@@ -1,4 +1,12 @@
+import MultiCartCore
 
+#if canImport(SwiftData)
+import SwiftData
 
-public enum MultiCartSwiftDataStorageMarker {}
+/// Entry point for the SwiftData-based CartStore implementation.
+@available(iOS 17, *)
+public struct SwiftDataCartStore {
+    public init() { }
+}
+#endif // canImport(SwiftData)
 
