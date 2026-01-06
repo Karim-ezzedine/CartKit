e8745c06faacc8569a122455b6ddeb660c25d474
Karim Ezzedinne <karim.ezz70@gmail.com>
2026-01-06 23:16:46 +0200

    fix CI error

--- Sources/CartKit/Factories/CartStoreFactory.swift
+++ Sources/CartKit/Factories/CartStoreFactory.swift
@@ -8,38 +8,55 @@ import CartKitStorageSwiftData
 
 public enum CartStoreFactory {
 
+    /// Always-available factory (macOS CI-safe).
+    /// - Note: SwiftData is not referenced here to keep CartKit buildable on macOS.
+    public static func makeStore(
+        preference: CartStoragePreference,
+        coreData: CoreDataCartStoreConfiguration = .init()
+    ) async throws -> any CartStore {
+
+        switch preference {
+
+        case .coreData:
+            return try await CoreDataCartStore(configuration: coreData)
+
+        case .swiftData:
+            // On platforms where SwiftData isn't available, we fail explicitly.
+            throw CartStoreFactoryError.swiftDataUnavailable
+
+        case .automatic:
+            // Default to Core Data unless the iOS 17+ overload is used.
+            return try await CoreDataCartStore(configuration: coreData)
+        }
+    }
+}
+
+#if os(iOS) && canImport(SwiftData) && canImport(CartKitStorageSwiftData)
+extension CartStoreFactory {
+
+    /// iOS 17+ factory that can build SwiftData stores.
+    @available(iOS 17, *)
     public static func makeStore(
         preference: CartStoragePreference,
         coreData: CoreDataCartStoreConfiguration = .init(),
         swiftData: SwiftDataCartStoreConfiguration = .init()
     ) async throws -> any CartStore {
-        
+
         switch preference {
-            
+
         case .coreData:
             return try await CoreDataCartStore(configuration: coreData)
 
         case .swiftData:
-            #if os(iOS) && canImport(SwiftData) && canImport(CartKitStorageSwiftData)
-            if #available(iOS 17, *) {
-                return try SwiftDataCartStore(configuration: swiftData)
-            } else {
-                throw CartStoreFactoryError.swiftDataUnavailable
-            }
-            #else
-            throw CartStoreFactoryError.swiftDataUnavailable
-            #endif
+            return try SwiftDataCartStore(configuration: swiftData)
 
         case .automatic:
-            #if os(iOS) && canImport(SwiftData) && canImport(CartKitStorageSwiftData)
-            if #available(iOS 17, *) {
-                return try SwiftDataCartStore(configuration: swiftData)
-            }
-            #endif
-            return try await CoreDataCartStore(configuration: coreData)
+            // Prefer SwiftData when available on this runtime.
+            return try SwiftDataCartStore(configuration: swiftData)
         }
     }
 }
+#endif
 
 enum CartStoreFactoryError: Error {
     case swiftDataUnavailable
